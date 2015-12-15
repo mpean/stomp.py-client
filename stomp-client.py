4cdd78340c1e3a851ec961a93b13b956eb6daf20
Maxime Pean <maxpean@gmail.com>
2015-12-15 14:46:43 +0100

    test

--- stomp-client.py
+++ stomp-client.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 import sys
 
 import stomp
@@ -9,14 +10,15 @@ class MyListener(stomp.ConnectionListener):
     def on_message(self, headers, message):
         print('received a message "%s"' % message)
 
-conn = stomp.Connection()
-conn.set_listener('', MyListener())
+#c = stomp.Connection([('127.0.0.1', 62613)])
+conn = stomp.Connection([('127.0.0.1', 61613)])
+#conn.set_listener('', MyListener())
 conn.start()
 conn.connect('user', 'user', wait=True)
 
-conn.subscribe(destination='/queue/test', id=1, ack='auto')
-
+#conn.subscribe(destination='/queue/test', id=1, ack='auto')
+print('emission "%s"' % datetime.now())
 conn.send(body=' '.join(sys.argv[1:]), destination='/queue/test')
 
-time.sleep(2)
+#time.sleep(2)
 conn.disconnect()
