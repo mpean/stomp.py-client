7164d5deb8a6e23a3373852f1faa38503d74ec07
Maxime Pean <maxpean@gmail.com>
2015-12-15 16:16:33 +0100

    test

--- stomp-client.py
+++ stomp-client.py
@@ -1,8 +1,9 @@
 import time
-from datetime import datetime
+import datetime
 import sys
 
 import stomp
+import json
 
 class MyListener(stomp.ConnectionListener):
     def on_error(self, headers, message):
@@ -17,8 +18,15 @@ conn.start()
 conn.connect('user', 'user', wait=True)
 
 #conn.subscribe(destination='/queue/test', id=1, ack='auto')
-print('emission "%s"' % datetime.now())
-conn.send(body=' '.join(sys.argv[1:]), destination='/queue/test')
+
+message={}
+message["time"]=unicode(datetime.datetime.now())
+message["body"]=' '.join(sys.argv[1:])
+print(json.dumps(message))
+
+conn.send(body=json.dumps(message), destination='/queue/test')
+
+#conn.send(body=' '.join(sys.argv[1:]), destination='/queue/test')
 
 #time.sleep(2)
 conn.disconnect()
